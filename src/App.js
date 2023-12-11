b67771c21b3c8a9a5a6b1745d6f948996fb3b1c4
Robbie Meijer <github.rro88ie@xs4all.nl>
2023-12-11 12:28:14 +0100

    refactor routing, add footer

--- src/App.js
+++ src/App.js
@@ -12,15 +12,20 @@ const App = () => {
         <header className="blog__header">
           <Navigation />
         </header>
-        <aside className="blog__sidebar">
-          <BlogForm />
-        </aside>
-        <main className="blog__main-content">
-          <Routes>
-            <Route path="/" element={<BlogPreview />} />
-            <Route path="/blog" element={<BlogArchive />} />
-          </Routes>
-        </main>
+        <div className="blog__content">
+          <aside className="blog__sidebar">
+            <Routes>
+              <Route path="/" element={<BlogForm />} />
+            </Routes>
+          </aside>
+          <main className="blog__main-content">
+            <Routes>
+              <Route path="/" element={<BlogPreview />} />
+              <Route path="/blog" element={<BlogArchive />} />
+            </Routes>
+          </main>
+        </div>
+        <footer className="blog__footer">copyright text</footer>
       </div>
     </Router>
   );
