f5daf144d9598b516ef78a81dfb5672ff1e1eaf4
Robbie Meijer <github.rro88ie@xs4all.nl>
2023-12-30 17:39:16 +0100

    undo add fallback showrequiredfieldsnotification

--- src/functions/showRequiredFieldsNotification.js
+++ src/functions/showRequiredFieldsNotification.js
@@ -1,9 +1,6 @@
 const showRequiredFieldsNotification = (fields = []) => {
   // Loop through every required field.
   fields?.forEach(({ fieldState, fieldElement, fieldName }) => {
-    // Fallback.
-    if (!fieldState || !fieldElement || !fieldName) return;
-
     // Clear existing error message and red border.
     fieldElement.current?.classList?.remove('required-field');
     fieldElement.current?.nextSibling?.remove();
