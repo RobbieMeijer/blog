edd166048f50a6f4d174d121da2225de1602a5e4
Robbie Meijer <github.rro88ie@xs4all.nl>
2024-01-02 02:07:55 +0100

    naviation: add active link toggle + styling

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,17 +1,33 @@
 import './style.scss';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 const Navigation = () => {
+  const location = useLocation();
+  const [activeLink, setActiveLink] = useState('/');
+
+  useEffect(() => {
+    setActiveLink(location.pathname);
+  }, [location]);
+
   return (
     <nav className="menu">
       <ul className="menu__list">
         <li className="menu__list-item">
-          <Link className="menu__link" to="/">
+          <Link
+            className={`menu__link ${'/' === activeLink ? 'active' : ''}`}
+            to="/"
+            onClick={() => setActiveLink('/')}
+          >
             Home
           </Link>
         </li>
         <li className="menu__list-item">
-          <Link className="menu__link" to="/blog">
+          <Link
+            className={`menu__link ${'/blog' === activeLink ? 'active' : ''}`}
+            to="/blog"
+            onClick={() => setActiveLink('/blog')}
+          >
             Blog
           </Link>
         </li>
