b2d6dcb60c0287a218d8f5d9c4aef59163260c5c
Robbie Meijer <github.rro88ie@xs4all.nl>
2024-01-02 17:45:13 +0100

    refactor camera icon blog form

--- src/components/BlogForm/index.js
+++ src/components/BlogForm/index.js
@@ -3,6 +3,7 @@ import { useEffect, useRef, useState } from 'react';
 import Button from '../Button';
 import showRequiredFieldsNotification from '../../functions/showRequiredFieldsNotification';
 import someFieldsAreEmpty from '../../functions/someFieldsAreEmpty';
+import blogIconCamera from '../../assets/blog-icon-camera.svg';
 
 const BlogForm = () => {
   // State.
@@ -184,20 +185,13 @@ const BlogForm = () => {
               ref={fileInputElement}
               className="blog-form__image-upload-container"
             >
-              <svg
+              <img
+                src={blogIconCamera}
+                alt="icon camera"
                 className="blog-form__image-upload-icon"
                 aria-hidden="true"
                 focusable="false"
-                xmlns="http://www.w3.org/2000/svg"
-                height="1em"
-                width="1em"
-                viewBox="0 0 512 512"
-              >
-                <path
-                  fill="currentColor"
-                  d="M149.1 64.8L138.7 96H64C28.7 96 0 124.7 0 160V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V160c0-35.3-28.7-64-64-64H373.3L362.9 64.8C356.4 45.2 338.1 32 317.4 32H194.6c-20.7 0-39 13.2-45.5 32.8zM256 192a96 96 0 1 1 0 192 96 96 0 1 1 0-192z"
-                />
-              </svg>
+              />
               <label
                 className="blog-form__image-upload-button"
                 htmlFor="imageUploadInput"
